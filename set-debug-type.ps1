2204e33bd0661e10bc6caa27d419fc438b94ca4b
ConfigCat <37753260+configcat@users.noreply.github.com>
2018-11-29 22:28:48 +0100

    fix Debug type location

--- set-debug-type.ps1
+++ set-debug-type.ps1
@@ -2,5 +2,5 @@ param($projectPath)
 
 $csprojPath = Join-Path $PSScriptRoot $projectPath
 [xml]$project = Get-Content -Path $csprojPath
-$project.Project.PropertyGroup[0].DebugType = "full"
+$project.Project.PropertyGroup.DebugType = "full"
 $project.Save($csprojPath)

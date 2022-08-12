0e9cc54668f65f819d09bc31d57c30fbdd8b7099
Zoltan David <zoltan.david@configcat.com>
2022-08-12 11:41:40 +0200

    removes lock + adds back threadsafe printing

--- src/ConfigCatClient/Logging/ConsoleLogger.cs
+++ src/ConfigCatClient/Logging/ConsoleLogger.cs
@@ -7,8 +7,6 @@ namespace ConfigCat.Client
     /// </summary>
     public class ConsoleLogger : ILogger
     {
-        private object _lock = new object();
-
         /// <inheritdoc />
         public LogLevel LogLevel { get; set; }
 
@@ -52,23 +50,17 @@ namespace ConfigCat.Client
 
         private void PrintMessage(LogLevel logLevel, string message)
         {
-            lock (this._lock)
+            const string defaultColorCode = "\u001B[36m";
+            const string resetCode = "\u001B[0m";
+            var logLevelColorCode = logLevel switch
             {
-                var originalColor = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("ConfigCat.");
-                switch (logLevel)
-                {
-                    case LogLevel.Error: Console.ForegroundColor = ConsoleColor.Red; break;
-                    case LogLevel.Warning: Console.ForegroundColor = ConsoleColor.Yellow; break;
-                    case LogLevel.Info: Console.ForegroundColor = ConsoleColor.Green; break;
-                    case LogLevel.Debug: Console.ForegroundColor = ConsoleColor.Blue; break;
-                }
-                Console.Write("{0,-7}", logLevel.ToString().ToUpper());
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine($" {message}");
-                Console.ForegroundColor = originalColor;
-            }
+                LogLevel.Error => "\u001B[31m",
+                LogLevel.Warning => "\u001B[33m",
+                LogLevel.Info => "\u001B[32m",
+                _ => "\u001B[32m"
+            };
+
+            Console.WriteLine($"{logLevelColorCode}CONFIGCAT {logLevel.ToString().ToUpper(),-7} {defaultColorCode}{message}{resetCode}");
         }
     }
 }

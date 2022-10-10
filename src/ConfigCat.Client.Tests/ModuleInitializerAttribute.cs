1c001c69188fd90f6f47fa9b5c87f00e7942b45a
Adam Simon <adamosimoni@gmail.com>
2022-10-10 11:16:06 +0200

    Updates LICENSE to mention including of ModuleInitializerAttribute

--- src/ConfigCat.Client.Tests/ModuleInitializerAttribute.cs
+++ src/ConfigCat.Client.Tests/ModuleInitializerAttribute.cs
@@ -1,8 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-// Source: https://github.com/dotnet/runtime/blob/v6.0.9/src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/ModuleInitializerAttribute.cs
-
 #if !NET5_0_OR_GREATER
 
 namespace System.Runtime.CompilerServices

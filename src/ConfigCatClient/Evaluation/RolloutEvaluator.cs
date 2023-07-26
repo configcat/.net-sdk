0a3b954750f00e46d3e4050895a1283d18fbe99e
Adam Simon <adamosimoni@gmail.com>
2023-07-26 09:24:55 +0200

    Format code

--- src/ConfigCatClient/Evaluation/RolloutEvaluator.cs
+++ src/ConfigCatClient/Evaluation/RolloutEvaluator.cs
@@ -1,8 +1,8 @@
-using ConfigCat.Client.Versioning;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using ConfigCat.Client.Versioning;
 
 using static System.FormattableString;
 

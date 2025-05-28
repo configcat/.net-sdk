08771dce96f2ed7695131537af50b6b11059913c
Adam Simon <adamosimoni@gmail.com>
2025-05-28 18:01:07 +0200

    Remove unnecessary usings

--- src/ConfigCat.Client.Tests/VariationIdTests.cs
+++ src/ConfigCat.Client.Tests/VariationIdTests.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
 using ConfigCat.Client.ConfigService;
--- src/ConfigCatClient/IConfigCatClient.cs
+++ src/ConfigCatClient/IConfigCatClient.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using ConfigCat.Client.Configuration;
 
 namespace ConfigCat.Client;
 

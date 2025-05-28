0c58e89e06e24c71fcf25dd970b557b4dc36bd3d
Adam Simon <adamosimoni@gmail.com>
2025-05-28 18:01:07 +0200

    Remove unnecessary usings

--- src/ConfigCatClient/Models/Segment.cs
+++ src/ConfigCatClient/Models/Segment.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
 using ConfigCat.Client.Evaluation;
 using ConfigCat.Client.Utils;

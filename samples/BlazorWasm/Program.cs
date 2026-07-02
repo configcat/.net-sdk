55b749b11cd7dedf4ffefc8fad98872511226552
Adam Simon <adamosimoni@gmail.com>
2026-07-02 13:42:26 +0200

    Improve Blazor sample app

--- samples/BlazorWasm/Program.cs
+++ samples/BlazorWasm/Program.cs
@@ -1,8 +1,10 @@
 using BlazorWasm;
 using ConfigCat.Client;
 using ConfigCat.Extensions.Hosting;
+using ConfigCat.Extensions.Hosting.Configuration;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Options;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -20,11 +22,23 @@ builder.Services.AddConfigCat(builder.Configuration, configCatBuilder => configC
     })
     .UseInitMode(new ConfigCatInitMode.WaitForClientReady(throwOnFailure: false)));
 
+// In browser applications, it's recommended to configure the ConfigCat client to use the browser's
+// localStorage API for caching config data to reduce ConfigCat network traffic. However, this is a
+// bit tricky in Blazor because we need to get the IJSRuntime service from the DI container to configure
+// the client options. This can be achieved using a custom IConfigureOptions<ExtendedConfigCatClientOptions>
+// implementation:
+var configureClientToUseLocalStorage = new LocalStorageConfigCache.ConfigureClientOptions();
+builder.Services.AddSingleton<IConfigureOptions<ExtendedConfigCatClientOptions>>(configureClientToUseLocalStorage);
+
 builder.Logging.SetMinimumLevel(builder.HostEnvironment.IsDevelopment()
     ? Microsoft.Extensions.Logging.LogLevel.Information
     : Microsoft.Extensions.Logging.LogLevel.Warning);
 
 var host = builder.Build();
+
+// Since JS interop is asynchronous, we need to take this extra initialization step before
+// the ConfigCat client is resolved from the DI container.
+await configureClientToUseLocalStorage.InitializeAsync(host.Services);
 
 // Usually, it's a good idea to ensure that the ConfigCat client is initialized before
 // the application starts, especially if you want to use synchronous feature flag evaluation.
